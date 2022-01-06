75f0b09c068bfa0f078a88255bff128d246ec401
carmem <carmem@cobot.me>
2022-01-06 12:20:55 -0300

    setting up basics

---
+++ _data/color.js
@@ -0,0 +1,113 @@
+module.exports = [
+  {
+    name: "blue-100",
+    color: "hsl(205, 75%, 11%)",
+  },
+  {
+    name: "blue-90",
+    color: "hsl(208, 81%, 22%)",
+    onColor: "hsl(0, 0%, 100%)",
+  },
+  {
+    name: "blue-80",
+    color: "hsl(210, 79%, 35%)",
+  },
+  {
+    name: "blue-70",
+    color: "hsl(205, 71%, 43%)",
+  },
+  {
+    name: "blue-60",
+    color: "hsl(208, 50%, 57%)",
+  },
+  {
+    name: "blue-50",
+    color: "hsl(208, 51%, 68%)",
+  },
+  {
+    name: "blue-40",
+    color: "hsl(211, 49%, 79%)",
+  },
+  {
+    name: "blue-30",
+    color: "hsl(209, 53%, 84%)",
+  },
+  {
+    name: "blue-20",
+    color: "hsl(210, 50%, 90%)",
+  },
+  {
+    name: "blue-10",
+    color: "hsl(217, 52%, 95%)",
+  },
+  {
+    name: "blue-4",
+    color: "hsl(204, 45%, 98%)",
+  },
+  {
+    name: "grey-100",
+    color: "hsl(229, 31%, 14%)",
+  },
+  {
+    name: "grey-90",
+    color: "hsl(229, 20%, 22%)",
+  },
+  {
+    name: "grey-80",
+    color: "hsl(229, 14%, 31%)",
+  },
+  {
+    name: "grey-70",
+    color: "hsl(229, 11%, 40%)",
+  },
+  {
+    name: "grey-60",
+    color: "hsl(229, 9%, 49%)",
+  },
+  {
+    name: "grey-50",
+    color: "hsl(229, 10%, 58%)",
+  },
+  {
+    name: "grey-40",
+    color: "hsl(229, 13%, 67%)",
+  },
+  {
+    name: "grey-30",
+    color: "hsl(230, 21%, 76%)",
+  },
+  {
+    name: "grey-20",
+    color: "hsl(229, 28%, 85%)",
+  },
+  {
+    name: "grey-10",
+    color: "hsl(228, 41%, 93%)",
+  },
+  {
+    name: "grey-4",
+    color: "hsl(230, 43%, 97%)",
+  },
+  {
+    name: "red-100",
+    color: "hsl(229, 9%, 49%)",
+  },
+  {
+    name: "sun",
+    color: "hsl(49, 94%, 58%)",
+  },
+  {
+    name: "mint",
+    color: "hsl(179, 74%, 39%)",
+    onColor: "hsl(348, 99%, 45%)",
+  },
+  {
+    name: "peach",
+    color: "hsl(179, 74%, 39%)",
+  },
+  {
+    name: "cream",
+    color: "hsl(36, 100%, 97%)",
+  },
+
+];
